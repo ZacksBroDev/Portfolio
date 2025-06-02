f72a79db53e63abf4480d8dedce9ef3484a4e056
Zackary Brown <zackaryzbrown@gmail.com>
2025-06-02 02:14:51 -0600

    added js for button funtion to hide and show text for about me page

---
+++ src/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll(".toggle-btn").forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    const index = parseInt(this.getAttribute("data-index"));
+    const details = document.querySelectorAll(".content-container .details");
+    // Hide all details first
+    details.forEach((d) => (d.style.display = "none"));
+    // Show the selected details
+    if (details[index]) {
+      details[index].style.display = "block";
+    }
+  });
+});
